6ec10122028d335b02e0b4f38493f33908fb1d3f
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-07-12 21:48:05 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,18 @@
+const dropdownList = document.querySelector(".dropdown__list"); 
+ const dropdownValue = document.querySelector(".dropdown__value"); 
+  
+ dropdownValue.addEventListener("click", dropdown); 
+  
+ function dropdown() { 
+     if (dropdownList.className.includes("dropdown__list_active")) { 
+         dropdownList.classList.remove("dropdown__list_active"); 
+     } else { 
+         dropdownList.classList.add("dropdown__list_active") 
+     } 
+ } 
+  
+ dropdownList.onclick = function (event) { 
+     event.preventDefault(); 
+     dropdownValue.textContent = event.target.textContent; 
+     dropdown(); 
+ };
