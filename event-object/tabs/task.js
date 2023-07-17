591e3b384b671fc0322030810121f2d728997e72
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-07-17 21:40:26 +0300

    Update task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,11 +1,20 @@
-const tabs = Array.from(document.getElementsByClassName("tab")); 
- const tabContent = Array.from(document.getElementsByClassName("tab__content")); 
-  
- tabs.forEach(tab => tab.addEventListener("click", click)); 
-  
- function click(event) { 
-     document.querySelector(".tab_active").classList.remove("tab_active"); 
-     document.querySelector(".tab__content_active").classList.remove("tab__content_active"); 
-     event.currentTarget.classList.add("tab_active"); 
-     tabContent[tabs.indexOf(event.currentTarget)].classList.add("tab__content_active"); 
- }
+let tabs = document.querySelectorAll('.tab');
+let tabContent = document.querySelectorAll('.tab__content');
+
+for (let tab of tabs) {
+    tab.addEventListener('click', changeTabs)
+}
+
+function changeTabs(e) {
+    for (let tab of tabs) {
+        tab.className = 'tab';
+    }
+    e.target.className = 'tab tab_active';
+
+    for (let content of tabContent) {
+        content.className = 'tab__content';
+    }
+    
+    const arrIndex = Array.from(tabs).indexOf(e.target);
+    tabContent[arrIndex].className = 'tab__content tab__content_active';
+}
