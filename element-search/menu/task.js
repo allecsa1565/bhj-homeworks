3cc0be69ce10725248b5d7bc214209f7163a0662
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-07-17 21:14:23 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,32 @@
-menuLinks.forEach(itemLink => { 
-     itemLink.onclick = () => { 
-         let menuActive = document.querySelectorAll(".menu_active"); 
-         if (itemLink.closest(".menu__item").querySelector(".menu_sub") != null) { 
-             itemLink.closest(".menu__item").querySelector(".menu_sub").classList.toggle("menu_active"); 
-             if (menuActive.length > 0) { 
-                 menuActive[0].classList.remove("menu_active"); 
-             } 
-             return false; 
-         } 
-     } 
- });
+'use strict';
+
+let menuLink = document.getElementsByClassName('menu__link');
+
+for (let i = 0; i < menuLink.length; i++) {
+	menuLink[i].onclick = () => checkMenu(i);
+};
+
+function checkMenu(i) {
+	let parent = menuLink[i].parentElement.querySelector('ul');
+
+	if (parent) {
+		let parentActive = parent.className.includes('menu_active');
+		change(menuLink[i]);
+
+		if (parentActive) {
+			parent.classList.remove('menu_active');
+		} else {
+			parent.classList.add('menu_active');
+		}
+	}
+
+	return false;
+}
+
+function change(element) {
+	let elementActive = element.closest('.menu_main').querySelector('.menu_active');
+
+	if(elementActive) {
+		elementActive.classList.remove('menu_active');
+	}
+}
