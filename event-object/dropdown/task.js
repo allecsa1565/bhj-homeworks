82fbc2fb2ce37ce21a6b4abbc027f02629cb8ff0
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-07-17 21:38:18 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,18 +1,23 @@
-const dropdownList = document.querySelector(".dropdown__list"); 
- const dropdownValue = document.querySelector(".dropdown__value"); 
-  
- dropdownValue.addEventListener("click", dropdown); 
-  
- function dropdown() { 
-     if (dropdownList.className.includes("dropdown__list_active")) { 
-         dropdownList.classList.remove("dropdown__list_active"); 
-     } else { 
-         dropdownList.classList.add("dropdown__list_active") 
-     } 
- } 
-  
- dropdownList.onclick = function (event) { 
-     event.preventDefault(); 
-     dropdownValue.textContent = event.target.textContent; 
-     dropdown(); 
- };
+'use strict'
+
+const dropDownValue = Array.from(document.getElementsByClassName('dropdown__value'));
+const dropDownList = Array.from(document.getElementsByClassName('dropdown__list'));
+
+function showMenu(){
+
+    dropDownValue.forEach(function(element,i){
+        document.addEventListener('click',function(event){
+            event.preventDefault();
+            if(event.target.classList.contains('dropdown__value')){
+                dropDownList[i].classList.add('dropdown__list_active');
+            }else{
+                if(event.target.classList.contains('dropdown__link')){
+                    const text = event.target.text;
+                    dropDownValue[i].textContent = text;
+                    dropDownList[i].classList.remove('dropdown__list_active');
+                }
+            }
+            
+        })        
+    });
+}
