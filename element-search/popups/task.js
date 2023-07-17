39c0ea05efbdbef4f90e24e2f0c0a20de6e7d0b1
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-07-17 21:18:54 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,21 +1,22 @@
-const mainModal = document.getElementById("modal_main"); 
- const successModal = document.getElementById("modal_success"); 
-  
- document.addEventListener("DOMContentLoaded", function(event) { 
-     mainModal.classList.add('modal_active'); 
- }); 
-  
- const close = Array.from(document.getElementsByClassName("modal__close")); 
- const success = Array.from(document.getElementsByClassName("show-success")); 
-  
- function closePopup() { 
-     this.closest(".modal_active").classList.remove("modal_active"); 
- } 
-  
- function successPopup() { 
-     mainModal.onclick = closePopup; 
-     successModal.classList.add('modal_active'); 
- } 
-  
- close.forEach(popup => popup.onclick = closePopup); 
- success.forEach(popup => popup.onclick = successPopup);
+'use strict';
+
+let modalMain = document.getElementById('modal_main');
+modalMain.className += ' modal_active';
+
+let modalClose = document.querySelector('div.modal__close');
+for(let i = 0; i < modalClose.length; i++) {
+	modalClose[i].onclick = () => moduleClose();
+}
+
+let modalSuccess = document.getElementById('modal_success');
+let showSuccess = document.querySelector('.show-success');
+
+showSuccess.onclick = () => {
+	moduleClose();
+	modalSuccess.className += ' modal_active';
+};
+
+function moduleClose () {
+	let elementActive = document.querySelector('.modal_active');
+	elementActive.className = elementActive.className.replace(' modal_active', '');
+}
