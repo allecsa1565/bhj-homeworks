f4d22ec1647b39715d8cb09633e7dc12e8665d0a
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-08-03 18:26:53 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,24 +1,34 @@
-const links = document.querySelectorAll('.has-tooltip'); 
- const hint = document.querySelector('.tooltip'); 
-  
- for (let link of links) { 
-     link.addEventListener('click', showHint); 
- } 
-  
- hint.addEventListener('click', () => hint.classList.remove('tooltip_active')); 
-  
- function showHint(event) { 
-  
-     event.preventDefault(); 
-  
-     const tooltip = document.querySelector('.tooltip_active'); 
-  
-     if (tooltip) { 
-         tooltip.classList.remove('tooltip_active'); 
-     } 
-  
-     hint.classList.add('tooltip_active'); 
-     hint.innerText = event.target.title; 
-     hint.style.top = event.target.getBoundingClientRect().top + 20 + 'px'; 
-     hint.style.left = event.target.getBoundingClientRect().left + 'px'; 
- }
+(function() {
+  const tooltips = Array.from(document.querySelectorAll(`.has-tooltip`));
+  tooltips.forEach((button, index) => {
+    let attr = button.getAttribute(`title`);
+    let posTooltipTop = button.getBoundingClientRect().top;
+    let posTooltipLeft = button.getBoundingClientRect().left;
+    let hint = creatHint(posTooltipTop, posTooltipLeft, attr);
+    button.insertAdjacentHTML(`afterEnd`, hint.outerHTML);
+    button.addEventListener(`click`, (elem => {
+      elem.preventDefault();
+      showHint(button, index);
+    }));
+  })
+}())
+
+function creatHint(posTop, posLeft, attribute) {
+  let hint = document.createElement(`div`);
+  hint.setAttribute(`class`, `tooltip`);
+  hint.setAttribute(`style`, `top: ${posTop + 20}px; left: ${posLeft}px`);
+  // hint.setAttribute(`style`, `top: 0; left: 0`);
+  // hint.style.top = posTooltipTop + 20 + `px`;
+  // hint.style.left = posTooltipLeft + `px`;
+  hint.textContent = attribute;
+  return hint;
+}
+
+function showHint(tooltip, index) {
+  const hints = Array.from(tooltip.closest(`body`).querySelectorAll(`.tooltip`));
+  let currentPosition = hints.findIndex(item => item.className.includes(`tooltip_active`));
+  if (currentPosition !== -1 && currentPosition !== index) {
+    hints[currentPosition].classList.remove(`tooltip_active`);
+  }
+  hints[index].classList.toggle(`tooltip_active`);
+}
