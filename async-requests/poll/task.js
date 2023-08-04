f4135b640ef4045a8af12268471c1989efd44780
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-08-04 23:07:51 +0300

    Update task.js

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,29 +1,28 @@
-const title = document.getElementById('poll__title'); 
- const answersList = document.getElementById('poll__answers'); 
-  
- const xhr = new XMLHttpRequest(); 
- xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php'); 
- xhr.addEventListener('readystatechange', show); 
- xhr.send(); 
-  
- function show(event) { 
-  
-     if (xhr.readyState === 4 && xhr.status === 200) { 
-  
-         const data = JSON.parse(xhr.responseText).data; 
-         title.innerText = data.title; 
-  
-         for (const answer of data.answers) { 
-  
-             const btn = `<button class="poll__answer">${answer}</button>`; 
-  
-             answersList.insertAdjacentHTML('beforeend', btn); 
-         } 
-  
-         const answerBtns = document.querySelectorAll('.poll__answer'); 
-  
-         for (const answerBtn of answerBtns) { 
-             answerBtn.addEventListener('click', () => alert('Спасибо, ваш голос засчитан!')); 
-         } 
-     } 
- }
+const pollTitle = document.getElementById('poll__title');
+const pollAnswer = document.getElementById('poll__answers');
+const xhRequest = new XMLHttpRequest();
+xhRequest.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+xhRequest.send();
+xhRequest.addEventListener('readystatechange', () => {
+
+    if (xhRequest.readyState === 4 && xhRequest.status === 200) {
+
+        const data = JSON.parse(xhRequest.responseText).data;
+        pollTitle.innerText = data.title;
+
+        for (const answer of data.answers) {
+
+            const button = `<button class="poll__answer">${answer}</button>`;
+
+            pollAnswer.insertAdjacentHTML('beforeend', button);
+        }
+
+        const answers = document.querySelectorAll('.poll__answer');
+        
+        for (const answer of answers) {
+            answer.addEventListener('click', () => {
+                alert('Спасибо, ваш вопрос засчитан!')
+            });
+        }
+    }
+});
