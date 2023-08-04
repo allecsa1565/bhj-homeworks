ddd5b5b665214648ac1edb5a748a249cc13237c7
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-08-04 23:12:30 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,29 @@
-const progress = document.getElementById('progress'); 
-  
- form.addEventListener('submit', check); 
-  
- function check(event) { 
-  
-     event.preventDefault(); 
-  
-     const form = document.getElementById('form'); 
-     const formData = new FormData(form); 
-  
-     const xhr = new XMLHttpRequest(); 
-     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php'); 
-     xhr.onprogress = function (event) { 
-  
-         progress.value = event.loaded / 1000000; 
-     } 
-     xhr.send(formData); 
-  
- }
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  const formData = new FormData(form);
+  const xhRequest = new XMLHttpRequest();
+  xhRequest.open('POST', ' https://netology-slow-rest.herokuapp.com/upload.php');
+  const progress = document.getElementById('progress');
+
+  xhRequest.upload.onloadstart = function() {
+
+    progress.value = 0.1
+
+    function increase() {
+
+      if(progress.value <= 0.9) {
+        progress.value = progress.value + 0.1;
+      }            
+    } 
+
+    setInterval(increase, 2000);
+  };  
+
+  xhRequest.upload.onload = function() {
+    progress.value = 1.0;
+  }
+    
+  xhRequest.send(formData);
+  e.preventDefault();
+});
