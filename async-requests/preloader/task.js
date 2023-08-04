a6aaabe42db1466fca4612cde73d59ad3c90298d
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-08-04 23:10:41 +0300

    Update task.js

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,37 +1,31 @@
-const loader = document.getElementById('loader'); 
- const list = document.getElementById('items'); 
-  
-  
- const xhr = new XMLHttpRequest(); 
- xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/'); 
- xhr.setRequestHeader('Content-Type', 'application/json'); 
- xhr.addEventListener('readystatechange', show); 
- xhr.send(); 
-  
-  
- function show(event) { 
-  
-     if (xhr.readyState === 4 && xhr.status === 200) { 
-  
-         loader.classList.remove('loader_active'); 
-  
-         const data = JSON.parse(xhr.responseText).response.Valute; 
-  
-         for (const valute in data) { 
-             const item = `<div class="item"> 
-                             <div class="item__code"> 
-                                 ${data[valute].CharCode} 
-                             </div> 
-                             <div class="item__value"> 
-                                 ${data[valute].Value} 
-                             </div> 
-                             <div class="item__currency"> 
-                                 руб. 
-                             </div> 
-                           </div>`; 
-  
-             list.insertAdjacentHTML('beforeend', item); 
-         } 
-  
-     } 
- }
+const loader = document.getElementById('loader');
+const items = document.getElementById('items');
+const xhrequest = new XMLHttpRequest();
+xhrequest.open('GET', 'https://netology-slow-rest.herokuapp.com/');
+xhrequest.setRequestHeader('Content-Type', 'application/json');
+xhrequest.send();
+xhrequest.addEventListener('readystatechange', ()=>{
+
+    if (xhrequest.readyState === 4) {
+
+        loader.classList.remove('loader_active');
+
+        const data = JSON.parse(xhrequest.responseText).response.Valute;
+
+        for (const valute in data) {
+            const item = `<div class="item">
+                            <div class="item__code">
+                                ${data[valute].CharCode}
+                            </div>
+                            <div class="item__value">
+                                ${data[valute].Value}
+                            </div>
+                            <div class="item__currency">
+                                руб.
+                            </div>
+                          </div>`;
+             items.insertAdjacentHTML('beforeend', item);
+        }
+
+    }
+});
