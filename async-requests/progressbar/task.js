d09140af3def5f0fd073f2ef803deceb94721799
allecsa1565 <135607167+allecsa1565@users.noreply.github.com>
2023-08-15 18:49:09 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,15 +1,17 @@
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const formData = new FormData(form);
   const xhRequest = new XMLHttpRequest();
-  xhRequest.open('POST',' https://students.netoservices.ru/nestjs-backend/upload');
+  xhRequest.open('POST',' https://students.netoservices.ru/nestjs-backend/upload'); xhRequest.setRequestHeader("Content-Type", "multipart/form-date");
   const progress = document.getElementById('progress');
 
   xhRequest.upload.onloadstart = function() {
-  xhr.upload.onprogress = event => { 
+  xhRequest.upload.onprogress = event => { 
      progress.value = event.loaded / event.total; 
-   };  xhRequest.upload.onload = function() {
+   };  
+  xhRequest.upload.onload = function() {
     progress.value = 1.0;
   }
     
